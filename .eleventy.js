ddff19566b89a2d0977b87e28b6488da3ea04fdf
PascalAmah <pascal.amah10@gmail.com>
2026-06-20 14:03:08 +0100

    build error

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 module.exports = function(eleventyConfig) {
   // Copy assets and css straight through to _site/
   eleventyConfig.addPassthroughCopy({ "src/css": "css" });
-  eleventyConfig.addPassthroughCopy({ "src/admin": "admin" });
+  eleventyConfig.addPassthroughCopy({ "src/admin/index.html": "admin/index.html" });
+  eleventyConfig.addPassthroughCopy({ "src/admin/config.yml": "admin/config.yml" });
   eleventyConfig.addPassthroughCopy("assets");
 
   return {
